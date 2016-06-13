994059210826e00eb2d6201ea8d6e48821a3b789
nicolaez <nzagaevschi@gmail.com>
2016-06-13 09:45:08 -0400

    a

--- application/models/Topping_model.php
+++ application/models/Topping_model.php
@@ -1,11 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-/**
- * Created by IntelliJ IDEA.
- * User: Nick
- * Date: 6/10/2016
- * Time: 6:58 PM
- */
+
 class Topping_model extends CI_Model{
 
     public function getAllTopings(){
