0c7ee3afaa3fbba9a0ffa70446766f726bc5370f
nicolaez <nzagaevschi@gmail.com>
2016-06-13 09:44:43 -0400

    a

--- application/models/Pizza_model.php
+++ application/models/Pizza_model.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Nick
- * Date: 6/10/2016
- * Time: 6:37 PM
- */
+
 class Pizza_model extends CI_Model{
 
     public function getAllTypeOfPizza(){
